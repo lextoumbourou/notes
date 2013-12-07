bc544249a252926ffc7443dc96a71cc71183b8dc
Lex Toumbourou <lextoumbourou@gmail.com>
2013-12-07 11:59:17 +1100

    Generate a feed

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Lex Toumbourou'
 SITENAME = u'LexToumbourou.com'
-SITEURL = ''
+SITEURL = 'http://lextoumbourou.com'
 
 TIMEZONE = 'Australia/Melbourne'
 
@@ -14,9 +14,8 @@ ARTICLE_URL = 'blog/posts/{slug}/'
 ARTICLE_SAVE_AS = 'blog/posts/{slug}/index.html'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
+FEED_DOMAIN = SITEURL
+FEED_ATOM = 'atom.xml'
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -31,4 +30,4 @@ STATIC_PATHS = ['images']
 DISQUS_SITENAME = 'lextoumbouroucom'
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
