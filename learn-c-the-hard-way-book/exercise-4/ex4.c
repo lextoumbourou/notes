81f333b2afbb8fe323f77944d59126121b8524e7
Lex Toumbourou <lextoumbourou@gmail.com>
2014-02-28 21:23:45 +1100

    Adding missing files

---
+++ learn-c-the-hard-way-book/exercise-4/ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 100;
+
+    printf("I am %d yrs old\n", age);
+    printf("I am %d inches tall", height);
+
+    return 0;
+}
