d52ade06c50d55e726617926544bea5be2d8ffdc
Lex Toumbourou <lextoumbourou@gmail.com>
2014-02-28 17:04:12 +1100

    Finished lesson 3

---
+++ learn-c-the-hard-way-book/exercise-3/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 100;
+    int height = 300;
+
+    printf("I am %d years old, innit?\n", age);
+    printf("I am %d cms tall\n", height);
+
+    return 0;
+}
