4e987db6a7c284f241782fbc18df885c426e7935
Lex Toumbourou <lextoumbourou@gmail.com>
2014-03-03 23:09:43 +1100

    Added exercise 7

---
+++ learn-c-the-hard-way-book/exercise-7/ex7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(int argc, char *argv[])
+{
+    int size_of_my_loud_pack = 50;
+    float t_per_gram = 10;
+
+    printf("Total t is %f\n", size_of_my_loud_pack * t_per_gram);
+}
