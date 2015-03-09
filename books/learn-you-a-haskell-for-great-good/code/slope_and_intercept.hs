fb82a99d176bd47819ca794e5abcf8b0521bd20d
Lex Toumbourou <lextoumbourou@gmail.com>
2015-03-09 12:47:26 +1100

    Slope value needs to be inversed.

--- books/learn-you-a-haskell-for-great-good/code/slope_and_intercept.hs
+++ books/learn-you-a-haskell-for-great-good/code/slope_and_intercept.hs
@@ -4,7 +4,7 @@ slope (x1, y1) (x2, y2) = (y2 - y1) / (x2 - x1)
 
 -- findIntercept: substitute in slope and attempt to find y-intercept
 findIntercept :: (Fractional a) => a -> a -> a -> a
-findIntercept slope x y = slope * x - y
+findIntercept slope x y = -1 * (slope * x - y)
 
 -- slopeAndIntercept: take in two coords and return a tuple: (slope, y-intercept)
 slopeAndIntercept :: (Fractional a) => (a, a) -> (a, a) -> (a, a)
