61a045ec7e39358b88bcbfcaf43b6c3bb9dee873
Lex Toumbourou <lextoumbourou@gmail.com>
2015-11-17 11:57:47 +1100

    Getting started with Rabbit.

---
+++ misc/rabbitmq/code/receive_ex1.py
@@ -0,0 +1,17 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    'localhost'))
+
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body)
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+channel.start_consuming()
