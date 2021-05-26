d259d79e63a1d6a6093e1cb9757ef09c6eb6c98a
Lex Toumbourou <lextoumbourou@gmail.com>
2021-05-27 00:12:56 +1000

    Another try.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PYTHON_VERSION=3.7 pip install -r requirements.txt
+export PYTHON_VERSION=3.7
+pip install -r requirements.txt
 git clone --recursive https://github.com/getpelican/pelican-plugins
 git clone https://github.com/VorpalBlade/pelican-cite.git
 
