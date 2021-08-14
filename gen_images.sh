e61a9d3514c73c4a6f32a49eff816cc439b1fc4f
Lex Toumbourou <lextoumbourou@gmail.com>
2021-08-14 19:20:43 +1000

    Fix colour of bracket.

--- gen_images.sh
+++ gen_images.sh
@@ -1,2 +1,2 @@
 manim --quality=m --save_as_gif code/manim/mm.py
-mv media/videos/mm/450p30/MatrixExamples_ManimCE_v0.9.0.gif notes/_media/matrix-multiplication.gif 
+mv media/videos/mm/400p30/MatrixExamples_ManimCE_v0.9.0.gif notes/_media/matrix-multiplication.gif 
