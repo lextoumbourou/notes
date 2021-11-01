206e7ad8141caaaf37c083d1698272b6f887cfac
Lex Toumbourou <lextoumbourou@gmail.com>
2021-11-01 22:08:17 +1000

    Remove newly added deps.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,4 @@ pip3 install -r requirements.txt
 git clone --recursive https://github.com/getpelican/pelican-plugins
 git clone https://github.com/VorpalBlade/pelican-cite.git
 
-node install
-
 ./build.sh
