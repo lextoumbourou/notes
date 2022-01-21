a304d58618db51bea5f28fcc91f4434c14ba0a73
Lex Toumbourou <lextoumbourou@gmail.com>
2022-01-21 18:27:40 +1000

    Publish first draft of Eigenvectors.

--- code/manim/eigenvectors.py
+++ code/manim/eigenvectors.py
@@ -9,16 +9,25 @@ from theme import Theme
 class EigenVectors(LinearTransformationScene):
     def __init__(self):
         super().__init__(
-            include_background_plane=False,
-            include_foreground_plane=False,
+            include_background_plane=True,
+            include_foreground_plane=True,
             show_basis_vectors=False
         )
 
     def construct(self):
         matrix = [[2, 1], [0, 2]]
 
-        self.add_vector((1, 0), animate=False, color=Theme.COLOR_1)
-        self.add_vector((0, 1), animate=False, color=Theme.COLOR_2)
+        ihat = self.add_vector((1, 0), animate=False, color=Theme.COLOR_1)
+        self.add_transformable_label(
+            ihat, r'\hat{i}',
+            new_label=r'\mathbf{A} \hat{j}',
+            animate=False, color=Theme.COLOR_1)
+
+        jhat = self.add_vector((0, 1), animate=False, color=Theme.COLOR_2)
+        self.add_transformable_label(
+            jhat, r'\hat{j}',
+            new_label=r'\mathbf{A} \hat{j}',
+            animate=False, color=Theme.COLOR_2)
 
         self.add_background_mobject(
             Line(start=array([0, 0, 0]), end=array([1, 0, 0]) * 10, stroke_color=Theme.COLOR_1))
