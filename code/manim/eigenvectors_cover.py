3e1feffdd77afb34151335f4fde8468022fe4d21
Lex Toumbourou <lextoumbourou@gmail.com>
2022-01-23 14:55:51 +1000

    Add cover for Eigenvalues and vectors.

---
+++ code/manim/eigenvectors_cover.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from manim import *
+
+from theme import Theme
+
+class EigenVectorCover(Scene):
+    def construct(self):
+        cross_product = MathTex(r"A", r"\vec{x}", r" = ", r"\lambda", r"\vec{x}", font_size=300).set_color(Theme.TEXT_COLOR)
+        cross_product.set_color_by_tex("{x}", Theme.COLOR_3)
+        cross_product.set_color_by_tex("{x}", Theme.COLOR_3)
+
+        self.add(cross_product)
+
+
+if __name__ == '__main__':
+    config.background_color = WHITE
+    config.format = 'gif'
+    config.output_file = Path(__file__).resolve().parent.parent.parent / Path('notes/_media/eigenvector-cover')
+    config.pixel_width = 400
+    config.pixel_height = 225
+
+    scene = EigenVectorCover()
+    scene.render()
--- notes/permanent/Eigenvalue.md
+++ notes/permanent/Eigenvalue.md
@@ -4,6 +4,7 @@ date: 2022-01-22 12:00
 tags:
   - LinearAlgebra
 summary: "A value which describes how much a transformation scales an Eigenvector"
+cover: /_media/eigenvalue-cover.png
 ---
 
 A scalar value that describes how much a transformation scales an [[Eigenvector]].
