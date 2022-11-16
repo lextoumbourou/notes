032a47878179b824294f5d3878816588f2fad5ce
Lex Toumbourou <lextoumbourou@gmail.com>
2022-11-16 20:20:40 +1000

    Try different Python version, I guess?

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PYTHON_VERSION=3.9
 pip3 install -r requirements.txt
 git clone --recursive https://github.com/getpelican/pelican-plugins
 git clone https://github.com/VorpalBlade/pelican-cite.git
