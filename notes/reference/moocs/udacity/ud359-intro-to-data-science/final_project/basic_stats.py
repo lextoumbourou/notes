51620e63883eb578dfd69e1fe5c96b4247e4c23f
Lex Toumbourou <lextoumbourou@gmail.com>
2023-04-10 18:25:54 +1000

    More.

--- notes/reference/moocs/udacity/ud359-intro-to-data-science/final_project/basic_stats.py
+++
@@ -1,8 +0,0 @@
-import pandas
-
-df = pandas.read_csv('./turnstile_data_master_with_weather.csv')
-daily = df['ENTRIESn_hourly'].sum() / 30
-
-print 'Total ridership: ', df['ENTRIESn_hourly'].sum()
-print 'Average per day: ', daily
-print 'Avergage per week: ', daily  * 7
