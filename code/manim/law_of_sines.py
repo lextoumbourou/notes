cb9ece8371ccb2daab05752f4e35717218043cca
Lex Toumbourou <lextoumbourou@gmail.com>
2023-08-05 13:59:45 +1000

    Wrote an article about the law of sines.

---
+++ code/manim/law_of_sines.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+from manim import *
+from numpy import array
+
+from theme import Theme
+
+config.background_color = WHITE
+config.save_as_gif = True
+config.pixel_height = 300
+config.pixel_width = 300
+config.medium_quality = True
+config.output_file = Path(__file__).resolve().parent.parent.parent / Path('notes/_media/law-of-sines-cover')
+
+config.frame_width = 6
+config.frame_height = 6
+
+LINE_COLOR = "#b9b28b"
+TEXT_COLOR = "#7187A2"
+
+line_args = {}
+
+class LawOfSinesTriangle(Scene):
+    def construct(self):
+        line_1 = Line(start=array([-2, -2.5, 0]), end=array([0, 1.5, 0]), **line_args)
+        line_1.set_color(Theme.TEXT_COLOR)
+
+        line_2 = Line(start=array([-2, -2.5, 0]), end=array([2, -1, 0]), **line_args)
+        line_2.set_color(Theme.TEXT_COLOR)
+
+        line_3 = Line(start=line_2.end, end=line_1.end, **line_args)
+        line_3.set_color(Theme.TEXT_COLOR)
+
+        angle_A = Angle(line_2, line_3, color=Theme.TEXT_COLOR, radius=0.7, other_angle=True, quadrant=(-1, 1))
+        angle_B = Angle(line_3, line_1, color=Theme.TEXT_COLOR, radius=0.7, other_angle=True, quadrant=(-1, -1))
+        angle_C = Angle(line_1, line_2, color=Theme.TEXT_COLOR, other_angle=True, radius=0.7)
+
+        self.add(line_1, line_2, line_3, angle_A, angle_B, angle_C)
+
+        equation = MathTex(r"\frac{\sin(A)}{a} = \frac{\sin(B)}{b} = \frac{\sin(C)}{c}", font_size=50)
+        equation.set_color(Theme.TEXT_COLOR)
+
+        equation[0][4].set_color(Theme.COLOR_2)
+        equation[0][7].set_color(Theme.COLOR_2)
+
+        equation[0][13].set_color(Theme.COLOR_3)
+        equation[0][16].set_color(Theme.COLOR_3)
+
+        equation[0][22].set_color(Theme.COLOR_1)
+        equation[0][25].set_color(Theme.COLOR_1)
+
+        equation.move_to(UP * 2.25)
+
+        self.add(equation)
+
+        self.add_label(line_1, 'a', Theme.COLOR_2, UP + LEFT)
+        self.add_label(line_2, 'b', Theme.COLOR_3, DOWN + RIGHT)
+        self.add_label(line_3, 'c', Theme.COLOR_1, UP + RIGHT)
+
+        self.add_angle_label(angle_A, 'A', Theme.COLOR_2, 0.2 * UP + 0.5 * LEFT)
+        self.add_angle_label(angle_B, 'B', Theme.COLOR_3, 0.4 * DOWN + 0 * LEFT)
+        self.add_angle_label(angle_C, 'C', Theme.COLOR_1, 0.35 * UP + 0.4 * RIGHT)
+
+    def add_label(self, line, text, color, direction):
+        label = Text(text)
+        label.set_color(color)
+        label.next_to(line.get_center(), direction=direction)
+        self.add(label)
+
+    def add_angle_label(self, angle, text, color, offset):
+        label = Text(text)
+        label.set_color(color)
+        label.move_to(angle.get_center() + offset)
+        self.add(label)
+
+
+if __name__ == '__main__':
+    scene = LawOfSinesTriangle()
+    scene.render()
--- notes/permanent/law-of-sines.md
+++ notes/permanent/law-of-sines.md
@@ -1,12 +1,79 @@
 ---
 title: Law of Sines
-date: 2023-07-02 00:00
-modified: 2023-07-02 00:00
-status: draft
+date: 2023-08-05 00:00
+tags:
+  - Maths
+  - Trigonometry
+  - PreLinearAlgebra
+cover: /_media/law-of-sines-cover.png
+summary: Tells us that the ratio between the sine of an angle and the side opposite it, will be constant
 ---
 
-The Law of Sines tells us that the ratio between the side of an angle, and the side opposite to it, will be constant for any angle in a triangle.
+The Law of Sines tells us that the ratio between the sine of an angle and the side opposite will be constant for any angle in a triangle.
 
-sin(C) / c = sin(A) / a = sin(B) / b
+$\frac{\sin C}{c} = \frac{\sin A}{a} = \frac{\sin B}{b}$
 
-#Maths/Trigonometry 
+If we have at least two sides and an angle or two angles and a side, we can use it to find the missing values.
+
+## Example
+
+**Question**
+
+Consider a triangle with angles $A$, $B$ and $C$ and sides $a$, $b$ and $c$, where $a$ is the side opposite $A$, $b$ opposite $B$ and $c$ opposite $C$.
+
+If $C = 42°$, $c = 15cm$ and $b = 1cm$, solve the triangle.
+
+**Working**
+
+We can start by finding $B$ using the Law of Sines:
+
+Since we know that $\frac{\sin 42°}{15cm} = \frac{\sin B}{1cm}$
+
+We can calculate $\frac{\sin 42°}{15} = 0.0446$
+
+$\frac{\sin B}{1} = 0.0446$ 
+
+Since dividing by 1 equals the numerator, we know:
+
+$\sin B = 0.0446$
+
+Then use $\arcsin$ to find $B$
+
+$B = arcsin(0.0446)$
+$B = 2.557°$
+
+We can now find $A$ since we know that all the angles in a triangle add up to $180°$
+
+$A = 180° - 42° - 2.56°$
+$A = 135.44°$
+
+Now to find $a$, we can use the Law of Sines again:
+
+$\frac{\sin 42°}{15cm} = \frac{\sin(135.44)}{a}$
+
+Calculate the known values:
+
+$0.0446 = 0.702 / a$
+
+Multiply both sides by A:
+
+$a \times 0.0446 = 0.702$
+$a = \frac{0.702}{0.0446} = 15.74cm$
+
+Now that we have the missing values, we can use the Law of Sines to check that all ratios are equal:
+
+$\frac{sin(135.44°)}{15.74} = 0.045$
+
+$\frac{sin(2.56°)}{1} = 0.045$
+
+$\frac{sin(42°)}{15} = 0.045$
+
+**Answer**
+
+$A = 135.44°$
+$B = 2.56°$
+$C = 42°$
+
+$a = 15.74cm$
+$b = 1cm$
+$c = 15cm$
