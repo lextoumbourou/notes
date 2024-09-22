e71afbe8f59dcb67d0be39b7ade87bc3414538e3
Lex Toumbourou <lex@canva.com>
2024-09-22 12:47:54 +1000

    Fix Waffle.

--- code/waffle_chart_cover.py
+++ code/waffle_chart_cover.py
@@ -5,10 +5,9 @@ from manim.theme import Theme
 
 fig = plt.figure(
     FigureClass=Waffle,
-    rows=5,
-    columns=5,
+    rows=3,
+    columns=3,
     values=[30, 16, 4],
     colors=[Theme.COLOR_1, Theme.COLOR_2, Theme.COLOR_3]
 )
 plt.savefig('notes/_media/waffle_cover.png', dpi=50, bbox_inches='tight')
-
