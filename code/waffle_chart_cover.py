13c4c4f060ceb724a5ea6aff3f766e1638d66fbd
Lex Toumbourou <lex@canva.com>
2024-09-22 13:06:46 +1000

    Improve cover image.

--- code/waffle_chart_cover.py
+++ code/waffle_chart_cover.py
@@ -5,9 +5,9 @@ from manim.theme import Theme
 
 fig = plt.figure(
     FigureClass=Waffle,
-    rows=3,
-    columns=3,
+    rows=9,
+    columns=9,
     values=[30, 16, 4],
     colors=[Theme.COLOR_1, Theme.COLOR_2, Theme.COLOR_3]
 )
-plt.savefig('notes/_media/waffle_cover.png', dpi=50, bbox_inches='tight')
+plt.savefig('notes/_media/waffle_cover.png', dpi=25, bbox_inches='tight')
