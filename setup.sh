75e292e8f84a8f36a75c026efca4f79989c74143
Lex Toumbourou <lex@canva.com>
2026-02-24 17:18:18 +0800

    Run npm install in setup.

--- setup.sh
+++ setup.sh
@@ -10,6 +10,16 @@ fi
 echo "Installing dependencies with uv..."
 uv sync
 
+# Install npm dependencies (mermaid-cli, puppeteer)
+echo "Installing npm dependencies..."
+npm install
+
+# Set Puppeteer to use Chromium from netlify-plugin-chromium
+if [ -n "$CHROME_PATH" ]; then
+    echo "Setting PUPPETEER_EXECUTABLE_PATH to $CHROME_PATH"
+    export PUPPETEER_EXECUTABLE_PATH="$CHROME_PATH"
+fi
+
 # Clone pelican-plugins repository
 git clone --recursive https://github.com/getpelican/pelican-plugins
 
